fa6809e8c4599476a87bb91c8448803f64454790
-1 <57960431+adma05@users.noreply.github.com>
2024-04-22 09:59:23 +0200

    Update require.cmd

--- require.cmd
+++ require.cmd
@@ -1,1 +1,1 @@
-npm i path && npm i url && npm i fs && npm i child_process && npm i electron && npm i os && npm i electron-packager && npm i electron --save
+npm init && npm i path && npm i url && npm i fs && npm i child_process && npm i electron && npm i os && npm i electron-packager && npm i electron --save
