7f69c5d427ab3bc374f45c087d937a9548e1e765
-1 <57960431+adma05@users.noreply.github.com>
2024-04-22 09:56:13 +0200

    Create require.cmd

---
+++ require.cmd
@@ -0,0 +1,36 @@
+# Windows CMD Remote Access by Telegram API
+This application will allow you to send cmd commands via Telegram
+<br>
+<br>
+<br>
+> [!IMPORTANT]
+> **Step 1** <br>
+> Enter each command in turn into the console (cmd), opening it in the application folder **or** run *require.cmd*
+
+```
+npm i fs
+npm i path
+npm i url
+npm i child_process
+npm i os 
+
+npm i electron
+npm i electron-packager
+npm i electron --save
+```
+
+
+> [!NOTE]
+> **Step 2** <br>
+> Run ``build.bat``
+
+
+> [!WARNING]
+> **Step 2.5** <br>
+> Copy "assets" in **release-builds**
+
+
+> [!NOTE]
+> **Step 3** <br>
+> Go in **release-builds** AND run app "console.exe"
+
